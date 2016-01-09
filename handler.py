502e8d1ef0bda42ef3e03cb377cf3198c5e41b93
jennifer lyden <jlyden1@alumni.nd.edu>
2016-01-08 20:21:50 -0500

    feat: Add Facebook Sign in

--- handler.py
+++ handler.py
@@ -53,7 +53,7 @@ def register():
 @app.route('/gconnect', methods=['POST'])
 def gconnect():
     if request.args.get('state') != login_session['state']:
-        connectError('Invalid state parameter', 401)
+        connectMessage('Invalid state parameter', 401)
     code = request.data
 
     # Upgrade auth code into credentials object
@@ -62,7 +62,7 @@ def gconnect():
         oauth_flow.redirect_uri = 'postmessage'
         credentials = oauth_flow.step2_exchange(code)
     except FlowExchangeError:
-        connectError('Failed to upgrade the authorization code.', 401)
+        connectMessage('Failed to upgrade the authorization code.', 401)
 
     # Check access token is valid
     access_token = credentials.access_token
@@ -71,22 +71,22 @@ def gconnect():
     result = json.loads(h.request(url, 'GET')[1])
     # If there was error in access token info, abort
     if result.get('error') is not None:
-        connectError(result.get('error'), 500)
+        connectMessage(result.get('error'), 500)
 
     # Verify intended user presented access token
     gplus_id = credentials.id_token['sub']
     if result['user_id'] != gplus_id:
-        connectError("Token's user ID doesn't match given user ID.", 401)
+        connectMessage("Token's user ID doesn't match given user ID.", 401)
 
     # Verify access token valid for this app
     if result['issued_to'] != '906905857159-19l8lcp90j3utgq2itj4upgdjtjaaltm.apps.googleusercontent.com':
-        connectError("Token's client ID doesn't match app's.", 401)
+        connectMessage("Token's client ID doesn't match app's.", 401)
 
     # If pass above tests ...
     stored_credentials = login_session.get('credentials')
     stored_gplus_id = login_session.get('gplus_id')
     if stored_credentials is not None and gplus_id == stored_gplus_id:
-        connectError('Current user is already connected.', 200)
+        connectMessage('Current user is already connected.', 200)
 
     # Store access token in session for later use
     login_session['credentials'] = access_token
@@ -127,9 +127,109 @@ def gconnect():
 def gdisconnect():
     # Only disconnect a connected user
     access_token = login_session.get('credentials')
+    if access_token is None:
+        connectMessage('User not connected.', 401)
+    # Execute HTTP GET request to revoke token
+    url = 'https://accounts.google.com/o/oauth2/revoke?token=%s' % access_token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[0]
+    if result['status'] == '200':
+        connectMessage('Successfully disconnected.', 200)
+    else:
+        # For some reason, token was invalid
+        connectMessage('Failed to revoke token for given user.', 400)
+
+# Facebook connect
+@app.route('/fbconnect', methods=['POST'])
+def fbconnect():
+    if request.args.get('state') != login_session['state']:
+        connectMessage('Invalid state parameter', 401)
+    access_token = request.data
+
+    # Exchange client token for long-lived server-side token
+    app_id = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_id']
+    app_secret = json.loads(open('fb_client_secrets.json', 'r').read())['web']['app_secret']
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id=%s&client_secret=%s&fb_exchange_token=%s' % (app_id, app_secret, access_token)
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+
+    # User token to get user info from API
+    userinfo_url = "https://graph.facebook.com/v2.4/me"
+    # Strip expire tag from access token
+    token = result.split("&")[0]
+
+    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+
+    login_session['provider'] = 'facebook'
+    login_session['username'] = data['name']
+    login_session['email'] = data['email']
+    login_session['facebook_id'] = data['id']
+
+    # store access token for later logout
+    stored_token = token.split("=")[1]
+    login_session['access_token'] = stored_token
+
+    # Get user picture
+    url = 'https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&height=150&width=150' % token
+    h = httplib2.Http()
+    result = h.request(url, 'GET')[1]
+    data = json.loads(result)
+    login_session['picture'] = data['data']['url']
+
+    # Check if user exists in database
+    user_id = getGiverID(login_session['email'])
+    if not user_id:
+        user_id = createGiver(login_session)
+    login_session['user_id'] = user_id
+
+    output = ''
+    output += '<h1>Welcome, '
+    output += login_session['username']
+    output += '!</h1>'
+    output += '<img src="'
+    output += login_session['picture']
+    output += '" style = "width: 150px; height 150px; border-radius: 75px;'
+    output += ' -webkit-border-radius: 75px; -moz-border-radius: 75px;">'
+    flash("You are now logged in as %s" % login_session['username'])
+    print "Login complete!"
+    return output
+
+# Facebook disconnect
+@app.route('/fbdisconnect')
+def fbdisconnect():
+    facebook_id = login_session['facebook_id']
+    url = 'https://graph.facebook.com/%s/permissions' % facebook_id
+    h = httplib2.Http()
+    result = h.request(url, 'DELETE')[1]
+    return "You have been logged out."
+
+# Disconnect - calls provider disconnects to revoke access token & reset login_session
+@app.route('/disconnect')
+def disconnect():
+    if 'provider' in login_session:
+        if login_session['provider'] == 'google':
+            gdisconnect()
+            del login_session['gplus_id']
+            del login_session['credentials']
+        if login_session['provider'] == 'facebook':
+            fbdisconnect()
+            del login_session['facebook_id']
+        del login_session['username']
+        del login_session['email']
+        del login_session['picture']
+        del login_session['user_id']
+        del login_session['provider']
+        flash("You have successfully logged out.")
+        return redirect(url_for('welcome'))
+    else:
+        flash("You were not logged in!")
+        return redirect(url_for('welcome'))
 
 # Helper functions
-def connectError(message, code):
+def connectMessage(message, code):
         response = make_response(json.dumps(message), code)
         response.headers['Content-Type'] = 'application/json'
         return response
@@ -335,11 +435,6 @@ def deleteGift(rec_id, gift_id):
         return redirect(url_for('gifts'))
     else:
         return render_template('giftDelete.html', recipient = thisRecipient, gift = thisGift, rec_id = rec_id, gift_id = gift_id)
-
-
-#Fake Gifts
-#gifts = [{'id': '1', 'recipient_id':'1', 'name':'Fiorentina Scarf', 'description':'Oversized Plaid Scarf with Fringe', 'image':'http://cdn.saleoffaccessories.com/d1/fc/d1fc1373d0e5e01ae234dd2c222b4468/la-fiorentina-plush-plaid-fringe-scarf.jpg', 'link':'http://www.amazon.com/Fiorentina-Womens-Oversized-Plaid-Fringe/dp/B00TV4CF36/ref=sr_1_7', 'status': 'idea'}, {'id': '2', 'recipient_id':'1', 'name':"What to Expect When You're Expecting", 'description':"America's pregnancy bible", 'image':"https://upload.wikimedia.org/wikipedia/en/d/d6/What_to_Expect_When_You're_Expecting_Cover.jpg", 'link':'http://www.amazon.com/What-Expect-When-Youre-Expecting/dp/0761148574/ref=sr_1_1', 'status': 'idea'}, {'id': '3', 'recipient_id':'1', 'name':'Mama Bee Belly Butter', 'description':"Burt's Bees Belly Butter, 6.5 Ounce", 'image':'http://www.allbabyforbaby.com/image/cache/data/burts-bees/burts-bees-mama-bee-belly-butter-6-5-ounce-2-500x500.jpg', 'link':'http://www.amazon.com/gp/product/B00DM14TYC/ref=s9_simh_gw_p14_d0_i5', 'status': 'idea'}, {'id': '4', 'recipient_id':'1', 'name':'Total Body Pillow', 'description':'Supports hips, back, neck, and tummy', 'image':'http://s7d9.scene7.com/is/image/BedBathandBeyond/8000415038357p', 'link':'http://www.amazon.com/gp/product/B0000635WI/ref=s9_simh_gw_p121_d0_i3', 'status': 'idea'}, {'id': '5', 'recipient_id':'1', 'name':'Baby Sling Carrier', 'description':'For babies from birth to 35 lbs', 'image':'https://images-na.ssl-images-amazon.com/images/I/41QZfy9IkAL.jpg', 'link':'http://www.amazon.com/Original--Fashion-Comfortable-Innoo-Tech/dp/B0156HL9WE/ref=sr_1_4', 'status': 'idea'}]
-#gift = {'id': '1', 'recipient_id':'1', 'name':'Fiorentina Scarf', 'description':'Oversized Plaid Scarf with Fringe', 'image':'http://cdn.saleoffaccessories.com/d1/fc/d1fc1373d0e5e01ae234dd2c222b4468/la-fiorentina-plush-plaid-fringe-scarf.jpg', 'link':'http://www.amazon.com/Fiorentina-Womens-Oversized-Plaid-Fringe/dp/B00TV4CF36/ref=sr_1_7', 'status': 'idea'}
 
 
 if __name__ == '__main__':
